6db82321ce1e500f8b09ecf48588425369df0127
rudenia <45589318+rudenia@users.noreply.github.com>
2022-02-20 11:56:36 +0100

    add accordion on section about

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -1,2 +1,14 @@
 // Accordion for section About //
-document
+document.querySelectorAll('.about-open').forEach((item) =>
+    item.addEventListener('click', () => {
+        const parrent = item.parentNode;
+
+        if (parrent.classList.contains('about__content-active')) {
+            parrent.classList.remove('about__content-active');
+        } else {
+            document.querySelectorAll('.about__repute__block')
+                .forEach((child) => child.classList.remove('about__content-active'));
+            parrent.classList.add('about__content-active');
+        }
+    })
+)
